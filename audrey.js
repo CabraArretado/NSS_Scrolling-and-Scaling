2eec45fed9764788c246b3088470a0fc45d9e384
felipetempus <felipemouradev@gmail.com>
2020-04-23 14:25:55 -0400

    done

---
+++ audrey.js
@@ -0,0 +1,15 @@
+const audrey = document.getElementById("audrey")
+
+/*
+    Add an event listener to the `document` object to listen
+    for the "scroll" event.
+*/
+document.addEventListener("scroll", () => {
+    console.log(window.scrollY)
+    if ( window.scrollY > 150) {
+    audrey.style.width = `${window.scrollY / 3}px`}
+    if (window.scrollY > 400 ) {
+        audrey.style.height = `${window.scrollY / 4}px`}
+    }
+)
+    // setAttribute("style", `width: ${window.scrollY / 3}px; height: ${window.scrollY / 4}px;`))
